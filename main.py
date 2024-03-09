0774b0dae2cff45003008290fab57b92ab221dc5
Shubham Shinde <70743278+shubhamshnd@users.noreply.github.com>
2024-03-09 23:32:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,307 @@
+import logging
+import queue
+import threading
+import urllib.request
+from pathlib import Path
+from typing import List, NamedTuple
+from PIL import Image
+import av
+import io
+import cv2
+import numpy as np
+import streamlit as st
+import os
+from skimage import io 
+import cv2
+
+from streamlit_webrtc import (
+    RTCConfiguration,
+    WebRtcMode,
+    
+    webrtc_streamer,
+)
+
+HERE = Path(__file__).parent
+
+logger = logging.getLogger(__name__)
+
+
+# This code is based on https://github.com/streamlit/demo-self-driving/blob/230245391f2dda0cb464008195a470751c01770b/streamlit_app.py#L48  # noqa: E501
+def download_file(url, download_to: Path, expected_size=None):
+    # Don't download the file twice.
+    # (If possible, verify the download using the file length.)
+    if download_to.exists():
+        if expected_size:
+            if download_to.stat().st_size == expected_size:
+                return
+        else:
+            st.info(f"{url} is already downloaded.")
+            if not st.button("Download again?"):
+                return
+
+    download_to.parent.mkdir(parents=True, exist_ok=True)
+
+    # These are handles to two visual elements to animate.
+    weights_warning, progress_bar = None, None
+    try:
+        weights_warning = st.warning("Downloading %s..." % url)
+        progress_bar = st.progress(0)
+        with open(download_to, "wb") as output_file:
+            with urllib.request.urlopen(url) as response:
+                length = int(response.info()["Content-Length"])
+                counter = 0.0
+                MEGABYTES = 2.0 ** 20.0
+                while True:
+                    data = response.read(8192)
+                    if not data:
+                        break
+                    counter += len(data)
+                    output_file.write(data)
+
+                    # We perform animation by overwriting the elements.
+                    weights_warning.warning(
+                        "Downloading %s... (%6.2f/%6.2f MB)"
+                        % (url, counter / MEGABYTES, length / MEGABYTES)
+                    )
+                    progress_bar.progress(min(counter / length, 1.0))
+    # Finally, we remove these visual elements by calling .empty().
+    finally:
+        if weights_warning is not None:
+            weights_warning.empty()
+        if progress_bar is not None:
+            progress_bar.empty()
+
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+
+def main():
+    st.header("WebRTC demo")
+
+    pages = {
+        "Real time object detection (sendrecv)": app_object_detection,
+        "Object Detection From Image" : photo2,
+        
+    }
+    page_titles = pages.keys()
+
+    page_title = st.sidebar.selectbox(
+        "Choose the app mode",
+        page_titles,
+    )
+    st.subheader(page_title)
+
+    page_func = pages[page_title]
+    page_func()
+
+    
+
+    logger.debug("=== Alive threads ===")
+    for thread in threading.enumerate():
+        if thread.is_alive():
+            logger.debug(f"  {thread.name} ({thread.ident})")
+
+
+
+
+
+    
+
+def app_object_detection():
+   
+    MODEL_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.caffemodel"  # noqa: E501
+    MODEL_LOCAL_PATH = HERE / "./models/MobileNetSSD_deploy.caffemodel"
+    PROTOTXT_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.prototxt.txt"  # noqa: E501
+    PROTOTXT_LOCAL_PATH = HERE / "./models/MobileNetSSD_deploy.prototxt.txt"
+
+    CLASSES = [
+        "background",
+        "aeroplane",
+        "bicycle",
+        "bird",
+        "boat",
+        "bottle",
+        "bus",
+        "car",
+        "cat",
+        "chair",
+        "cow",
+        "diningtable",
+        "dog",
+        "horse",
+        "motorbike",
+        "person",
+        "pottedplant",
+        "sheep",
+        "sofa",
+        "train",
+        "tvmonitor",
+    ]
+
+    @st.experimental_singleton
+    def generate_label_colors():
+        return np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+    COLORS = generate_label_colors()
+
+    download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564)
+    download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353)
+
+    DEFAULT_CONFIDENCE_THRESHOLD = 0.5
+
+    class Detection(NamedTuple):
+        name: str
+        prob: float
+
+    # Session-specific caching
+    cache_key = "object_detection_dnn"
+    if cache_key in st.session_state:
+        net = st.session_state[cache_key]
+    else:
+        net = cv2.dnn.readNetFromCaffe(str(PROTOTXT_LOCAL_PATH), str(MODEL_LOCAL_PATH))
+        st.session_state[cache_key] = net
+
+    confidence_threshold = st.slider(
+        "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
+    )
+
+    def _annotate_image(image, detections):
+        # loop over the detections
+        (h, w) = image.shape[:2]
+        result: List[Detection] = []
+        for i in np.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+
+            if confidence > confidence_threshold:
+                # extract the index of the class label from the `detections`,
+                # then compute the (x, y)-coordinates of the bounding box for
+                # the object
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                name = CLASSES[idx]
+                result.append(Detection(name=name, prob=float(confidence)))
+
+                # display the prediction
+                label = f"{name}: {round(confidence * 100, 2)}%"
+                cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(
+                    image,
+                    label,
+                    (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    COLORS[idx],
+                    2,
+                )
+        return image, result
+
+    result_queue = (
+        queue.Queue()
+    )  
+
+    def callback(frame: av.VideoFrame) -> av.VideoFrame:
+        image = frame.to_ndarray(format="bgr24")
+        blob = cv2.dnn.blobFromImage(
+            cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+        )
+        net.setInput(blob)
+        detections = net.forward()
+        annotated_image, result = _annotate_image(image, detections)
+
+        # NOTE: This `recv` method is called in another thread,
+        # so it must be thread-safe.
+        result_queue.put(result)  # TODO:
+
+        return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
+
+    webrtc_ctx = webrtc_streamer(
+        key="object-detection",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_frame_callback=callback,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
+    )
+
+    if st.checkbox("Show the detected labels", value=True):
+        if webrtc_ctx.state.playing:
+            labels_placeholder = st.empty()
+            
+            while True:
+                try:
+                    result = result_queue.get(timeout=1.0)
+                except queue.Empty:
+                    result = None
+                labels_placeholder.table(result)
+
+
+
+
+def photo2():
+    
+    st.header("Object Detection using haarcascade")
+
+    img_data = st.file_uploader(label='Drag and Drop Image',type =['png','jpg','jpeg'])
+    
+    if img_data is not None :
+        
+        uploaded_image = Image.open(img_data)
+
+#save uploaded image locally
+        uploaded_image.save("./tempimage/userinput.jpg")
+        
+
+#display image
+        st.image(uploaded_image,caption = 'Your Image', use_column_width= True)
+
+        image2= io.imread("./tempimage/userinput.jpg")
+
+        
+        face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        #eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+        cat_cascade = cv2.CascadeClassifier("haarcascade_frontalcatface.xml")
+        faces = face_cascade.detectMultiScale(image2)
+        #eyes = eye_cascade.detectMultiScale(image2)
+        cats = cat_cascade.detectMultiScale(image2)
+        #print(f"{len(faces)} faces detected in the image.")
+        for x, y, width, height in faces:
+            cv2.rectangle(image2, (x, y), (x + width, y + height), color=(255, 0, 0), thickness=2)
+        #for x, y, width, height in eyes:
+            #cv2.rectangle(image2, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
+        for x, y, width, height in cats:
+            cv2.rectangle(image2, (x, y), (x + width, y + height), color=(0, 0, 255), thickness=2)
+
+        cv2.imwrite("./tempimage/userinput.jpg", image2)
+        
+    st.image(image2, use_column_width=True,clamp = True)
+    os.remove("./tempimage/userinput.jpg")
+
+
+
+    
+
+if __name__ == "__main__":
+    import os
+
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
+
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+
+    main()
